3160a0242b8195bbb419a1b11e49e0bd1035a492
HIGASHI Taiju <higashi@taiju.info>
2017-07-12 23:33:54 +0900

    (car (cdr ファンクション倶楽部))

---
+++ 2017/07/car-cdr-function-club/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'FP入門方法思案 with JavaScript', 
+     subtitle: '(car (cdr ファンクション倶楽部))', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/fp.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'taiju', 
+     company: 'esm', 
+     gplus: '', 
+     twitter: 'https://twitter.com/taiju', 
+     www: 'http://taiju.hatenablog.com/', 
+     github: 'https://github.com/taiju', 
+   }]
+};
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 taijuのスライド置き場。
 
 ## What's New
-- 2012.05 [Kyoto.lisp TT #1 - よりSchemerフレンドリなJavaScriptへ](http://taiju.github.com/slides/2012/05/kyoto.lisp/)
+- 2017.07 [(car (cdr ファンクション倶楽部)) - FP入門方法思案 with JavaScript](http://taiju.github.com/slides/2017/07/car-cdr-function-club/slide.html)
 - 2012.09 [kanazawa.rb meetup 2 - Mojoliciousと愉快なPerl](http://taiju.github.com/slides/2012/09/kanazawa.rb/)
+- 2012.05 [Kyoto.lisp TT #1 - よりSchemerフレンドリなJavaScriptへ](http://taiju.github.com/slides/2012/05/kyoto.lisp/)
